4d852512ad4c2643dbcef7d3fc8dc2eee6a16797
yumecode11 <yumecode11@gmail.com>
2024-11-08 00:41:18 +0700

    setup lib

---
+++ programs/bonding_curve/src/instructions/initialize.rs
@@ -0,0 +1,39 @@
+use crate::{errors::CustomError, state::*};
+use anchor_lang::prelude::*;
+
+pub fn initialize(
+    ctx: Context<InitializeCurveConfiguration>,
+    fees: f64,
+    target: f64
+) -> Result<()> {
+    let dex_config = &mut ctx.accounts.dex_configuration_account;
+
+    if fees < 0_f64 || fees > 100_f64 {
+        return err!(CustomError::InvalidFee);
+    }
+
+    if target < 1000000000_f64 {
+        return err!(CustomError::InvalidTarget);
+    }
+
+    dex_config.set_inner(CurveConfiguration::new(fees, target));
+
+    Ok(())
+}
+
+#[derive(Accounts)]
+pub struct InitializeCurveConfiguration<'info> {
+    #[account(
+        init,
+        space = CurveConfiguration::ACCOUNT_SIZE,
+        payer = admin,
+        seeds = [CurveConfiguration::SEED.as_bytes()],
+        bump,
+    )]
+    pub dex_configuration_account: Box<Account<'info, CurveConfiguration>>,
+
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+}
